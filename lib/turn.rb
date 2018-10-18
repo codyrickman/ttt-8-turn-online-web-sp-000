bc9b65ff2977a4f12019ac2276219e6905d02da8
Cody Rickman <cody@creativemindstudios.com>
2018-10-18 14:04:50 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,19 @@
+def turn (board)
+  turns = 0
+  while turns < 1 do
+    puts "Please enter 1-9:"
+    
+    input = gets.strip
+    
+    index = input_to_index(input)
+    
+    if valid_move?(board, index) == true
+      move(board, index)
+      turns += 1
+    end
+  end
+end
+
 def move(board, index, symbol = "X")
   board[index] = symbol
   display_board(board)
